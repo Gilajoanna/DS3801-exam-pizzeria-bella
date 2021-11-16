7f06735b8fd96c337e3f823518845f0c2b9d5992
saasins <sandrasins@gmail.com>
2021-11-16 21:36:56 +0100

    Adding buttons to PizzaList.js

--- src/components/PizzaList.js
+++ src/components/PizzaList.js
@@ -1,4 +1,6 @@
 import { Container, Fab, Grid } from '@mui/material';
+import { Typography } from '@mui/material';
+import Button from '@mui/material/Button';
 import { useHistory } from 'react-router';
 
 import PizzaCard from './PizzaCard';
@@ -12,15 +14,36 @@ export default function PizzaList({pizzaList, addPizzaToCart}) {
     }
     */
 
-
+    
     return (
+        <main>
+            <Typography>
+                component="h1"
+                variant="h3"
+            </Typography>
+
+            <Button sx={{ justifyContent: 'flex-end', color: '#1D1F2C', letterSpacing: '4px', fontSize: 20 }} 
+            className='pizzaGoToPsta' variant="contained" size="medium">PASTA
+            </Button>
+
+            <Button sx={{ justifyContent: 'flex-end', color: '#1D1F2C', letterSpacing: '4px', fontSize: 20 }} 
+            className='pizzaGoToSideDishes' variant="contained" size="medium">SIDE DISHES
+            </Button>
+
+            <Button sx={{ justifyContent: 'flex-end', color: '#1D1F2C', letterSpacing: '4px', fontSize: 20 }} 
+            className='pizzaGoToDrinks' variant="contained" size="medium">DRINKS
+             </Button>
+
+            <Container maxWidth="md"> 
+                <Grid container spacing={3}>
+                    {pizzaList.map((pizza, index) => {
+                        return <Grid item xs={12} lg={6} key={index}><PizzaCard pizza={pizza}/></Grid>
+                    })}
+                </Grid>
+            </Container>
+
+
+        </main>
         
-        <Container maxWidth="md"> 
-            <Grid container spacing={3}>
-                {pizzaList.map((pizza, index) => {
-                    return <Grid item xs={12} lg={6} key={index}><PizzaCard pizza={pizza}/></Grid>
-                })}
-            </Grid>
-        </Container>
     )
 }
