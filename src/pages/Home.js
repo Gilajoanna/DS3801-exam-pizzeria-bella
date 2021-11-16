2fcd48e5c68ca4eda546599d78db720662fcf4e2
Gilajoanna <gilajoannajohansson@gmail.com>
2021-11-16 17:47:22 +0100

    adding Home + connecting navbar

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Navbar from '..components/Navbar';
+
+function Home() {
+    return(
+        <Container maxWidth="md">
+            <Navbar/>
+        </Container>
+    )
+}
+
+export default Home;
