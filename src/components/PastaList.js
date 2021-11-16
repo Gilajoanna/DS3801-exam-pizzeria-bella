4cdaa88ec3537606e3750e015ea0aba2f5029988
saasins <sandrasins@gmail.com>
2021-11-16 19:45:53 +0100

    removed deleted files

---
+++ src/components/PastaList.js
@@ -0,0 +1,24 @@
+import { Container, Fab, Grid } from '@mui/material';
+import { useHistory } from 'react-router';
+
+import PastaCard from './PastaCard';
+
+export default function PastaList({pastaList, addPastaToCart}) {
+    /*
+    let history = useHistory();
+
+    const goToCartPasta = () => {
+        history.push("/addPastaToCart")
+    }
+    */
+
+    return (
+        <Container maxWidth="md"> 
+            <Grid container spacing={3}>
+                {pastaList.map((pasta, index) => {
+                    return <Grid item xs={12} lg={6} key={index}><PastaCard pasta={pasta}/></Grid>
+                })}
+            </Grid>
+        </Container>
+    )
+}
