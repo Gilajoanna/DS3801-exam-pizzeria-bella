4ebd41ef6000f2b4b0d6a08b13251f385b80d1ce
Gilajoanna <gilajoannajohansson@gmail.com>
2021-11-16 12:29:01 +0100

    editing navbar.js + adding style.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
-import { Toolbar, Typography } from "@mui/material";
-import { Link } from 'react-router-dom'
+import { Toolbar, Typography, IconButton } from "@mui/material";
+import Button from '@mui/material/Button';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { Link } from 'react-router-dom';
 import React from "react";
 
 function Navbar() {
@@ -9,13 +11,18 @@ function Navbar() {
             <Toolbar sx={{ mb: 2, borderBottom: 1, borderColor: 'divider' }}>
                 <Typography
                     component="h2"
-                    variant="h5"
+                    variant="h3"
                     color="inherit"
                     noWrap
-                    //sx={{ flex }}
-                >
-                Hej Hej    
-                </Typography>    
+                    sx={{ flex: 1 }}>
+                    Pizzeria Bella
+                </Typography>  
+                    <Button sx={{ justifyContent: 'flex-end' }} className='navbar-button' variant="text" size="medium">Order food</Button> 
+                    <Button sx={{ justifyContent: 'flex-end' }} className='navbar-button' variant="text" size="medium">Contact</Button>
+                    <Button sx={{ justifyContent: 'flex-end' }} className='navbar-button' variant="text" size="medium">About us</Button>
+                    <IconButton color="primary" aria-label="add to shopping cart">
+                        <AddShoppingCartIcon />
+                    </IconButton>
             </Toolbar>
         </navbar>
     )
