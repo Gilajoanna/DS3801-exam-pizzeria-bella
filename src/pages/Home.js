48ba330cd498e5912b7fb28df50adb4f60aefd8f
Gilajoanna <gilajoannajohansson@gmail.com>
2021-11-17 13:49:57 +0100

    adding navbar components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import { Container } from '@mui/material'
 import React from 'react';
 
-import Navbar from '../components/Navbar';
+import Navbar from '../components/navbar/Navbar';
 import HeroImg from '../components/HeroImg';
 import OrderOnline from '../components/OrderOnline';
 import '../style2.css';
