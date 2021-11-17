3e453e0ccb1736ecbdcd70af3652ebc7c6ecdd8b
saasins <sandrasins@gmail.com>
2021-11-17 10:29:23 +0100

    Added changes to OrderFood component and css

--- src/components/OrderFood.js
+++ src/components/OrderFood.js
@@ -1,30 +1,53 @@
-import { Container, Fab, Grid } from '@mui/material';
+import { Container, Box, Fab, Grid } from '@mui/material';
 import { Typography } from '@mui/material';
 import Button from '@mui/material/Button';
+
 
 
 
 export default function OrderFood(){
 
     return(
+        <Grid item xs={12}>
+            <Container maxWidth="md" className="orderFodd-container">
         
-        <Container maxWidth="md" className="orderFodd-container">
-    
-            <Typography sx={{ justifyContent: 'center', borderColor: '#1D1F2C'}}>
-                <h1>ORDER FOOD</h1>
-            </Typography>
-            <Button sx={{ justifyContent: 'center', color: '#1D1F2C', letterSpacing: '4px', fontSize: 22 }} 
-                className='orderFood-button' variant="outlined" size="large">PIZZA
-            </Button>
-            <Button sx={{ justifyContent: 'center', color: '#1D1F2C', letterSpacing: '4px', fontSize: 22 }} 
-                className='orderFood-button' variant="outlined" size="large">PASTA
-            </Button>
-            <Button sx={{ justifyContent: 'center', color: '#1D1F2C', letterSpacing: '4px', fontSize: 22 }} 
-                className='orderFood-button' variant="outlined" size="large">SIDE DISHES
-            </Button>
-            <Button sx={{ justifyContent: 'center', color: '#1D1F2C', letterSpacing: '4px', fontSize: 22 }} 
-                className='orderFood-button' variant="outlined" size="large">BEVERAGES
-            </Button>
-        </Container>
+                <Typography sx={{ justifyContent: 'center', borderColor: '#1D1F2C'}}>
+                    <h1 className="orderFodd-header">ORDER FOOD</h1>
+                </Typography>
+
+                <Grid item xs={6}>
+                    <Box sx={{ flexDirection: 'column', justifyContent: 'center' }} item xs={6}>
+                        <Button sx={{ color: '#1D1F2C', letterSpacing: '4px', fontSize: 28 }} 
+                            className='orderFood-button' variant="outlined"  size="large">PIZZA
+                        </Button>
+                    </Box>
+                </Grid>
+
+                <Grid item xs={6}>
+                    <Box sx={{ flexDirection: 'column', justifyContent: 'center' }} item xs={6}>
+                        <Button sx={{ color: '#1D1F2C', letterSpacing: '4px', fontSize: 28 }} 
+                            className='orderFood-button' variant="outlined" size="large">PASTA
+                        </Button>
+                    </Box>
+                </Grid>
+
+                <Grid item xs={6}>
+                    <Box sx={{ flexDirection: 'column', justifyContent: 'center' }} item xs={6}>
+                        <Button sx={{ color: '#1D1F2C', letterSpacing: '4px', fontSize: 28 }} 
+                            className='orderFood-button' variant="outlined" size="large">SIDE DISHES
+                        </Button>
+                    </Box>
+                </Grid>
+
+                <Grid item xs={6}>
+                    <Box sx={{ flexDirection: 'column', justifyContent: 'center' }} item xs={6}>
+                        <Button sx={{ color: '#1D1F2C', letterSpacing: '4px', fontSize: 28 }} 
+                            className='orderFood-button' variant="outlined" size="large">BEVERAGES
+                        </Button>
+                    </Box>
+                </Grid>
+                
+            </Container>
+        </Grid>
     )
 }
