2073115753a28e94256ca0f3da8fb716d5b48f32
Gilajoanna <gilajoannajohansson@gmail.com>
2021-11-17 09:24:37 +0100

    image is temporarily fixed

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -3,11 +3,8 @@ import React from 'react';
 
 function HeroImg(props) {
     return(
-        <section className='image-section'>
-            <Container>
-                <img sx={{ flex: 1 }} className="home-image" src={ props.img } alt='home_image' />
-            </Container>
-        </section>
+                <img className="home-image" src={ props.img } alt='home_image' />
+
     )
 }
 
