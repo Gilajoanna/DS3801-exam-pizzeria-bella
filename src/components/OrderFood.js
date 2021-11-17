3d02fbd48bbf01879c78be92989b72d1cc10e162
saasins <sandrasins@gmail.com>
2021-11-17 11:01:32 +0100

    added changes to OrderFood component

--- src/components/OrderFood.js
+++ src/components/OrderFood.js
@@ -8,39 +8,39 @@ import Button from '@mui/material/Button';
 export default function OrderFood(){
 
     return(
-        <Grid item xs={12}>
+        <Grid>
             <Container maxWidth="md" className="orderFodd-container">
         
-                <Typography sx={{ justifyContent: 'center', borderColor: '#1D1F2C'}}>
+                <Typography sx={{ justifyContent: 'center', textAlign: 'center'}}>
                     <h1 className="orderFodd-header">ORDER FOOD</h1>
                 </Typography>
 
-                <Grid item xs={6}>
-                    <Box sx={{ flexDirection: 'column', justifyContent: 'center' }} item xs={6}>
+                <Grid>
+                    <Box sx={{ flexDirection: 'column', justifyContent: 'center', textAlign: 'center' }} item xs={6}>
                         <Button sx={{ color: '#1D1F2C', letterSpacing: '4px', fontSize: 28 }} 
                             className='orderFood-button' variant="outlined"  size="large">PIZZA
                         </Button>
                     </Box>
                 </Grid>
 
-                <Grid item xs={6}>
-                    <Box sx={{ flexDirection: 'column', justifyContent: 'center' }} item xs={6}>
+                <Grid>
+                    <Box sx={{ flexDirection: 'column', justifyContent: 'center', textAlign: 'center' }} item xs={6}>
                         <Button sx={{ color: '#1D1F2C', letterSpacing: '4px', fontSize: 28 }} 
                             className='orderFood-button' variant="outlined" size="large">PASTA
                         </Button>
                     </Box>
                 </Grid>
 
-                <Grid item xs={6}>
-                    <Box sx={{ flexDirection: 'column', justifyContent: 'center' }} item xs={6}>
+                <Grid>
+                    <Box sx={{ flexDirection: 'column', justifyContent: 'center', textAlign: 'center' }} item xs={6}>
                         <Button sx={{ color: '#1D1F2C', letterSpacing: '4px', fontSize: 28 }} 
                             className='orderFood-button' variant="outlined" size="large">SIDE DISHES
                         </Button>
                     </Box>
                 </Grid>
 
-                <Grid item xs={6}>
-                    <Box sx={{ flexDirection: 'column', justifyContent: 'center' }} item xs={6}>
+                <Grid>
+                    <Box sx={{ flexDirection: 'column', justifyContent: 'center', textAlign: 'center' }} item xs={6}>
                         <Button sx={{ color: '#1D1F2C', letterSpacing: '4px', fontSize: 28 }} 
                             className='orderFood-button' variant="outlined" size="large">BEVERAGES
                         </Button>
