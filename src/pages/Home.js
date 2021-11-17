3254abafbb309755ff89a1aba058fe4009316fd2
saasins <sandrasins@gmail.com>
2021-11-17 08:45:42 +0100

    Added backgroundcolor to Order Food component + made order foor component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,14 @@ import React from 'react';
 
 import Navbar from '../components/Navbar';
 import HeroImg from '../components/HeroImg';
+import OrderFood from '../components/OrderFood';
 
 function Home(props) {
     return(
         <Container maxWidth="xl">
             <Navbar />
             <HeroImg img={ props.img } />
+            <OrderFood/>
         </Container>
     )
 }
