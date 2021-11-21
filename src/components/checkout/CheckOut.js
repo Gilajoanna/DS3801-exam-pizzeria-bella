2e614ebc10c282149602723d1118390a57f3fe5d
Gilajoanna <gilajoannajohansson@gmail.com>
2021-11-21 11:58:10 +0100

    check out buttons

--- src/components/checkout/CheckOut.js
+++ src/components/checkout/CheckOut.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Typography, Button, Grid } from "@mui/material";
 
 import Navbar from '../navbar/Navbar';
 
@@ -10,15 +10,26 @@ function CheckOut() {
             <Navbar />
             <Container className="checkout-container" maxWidth="xl">
                 <Box className="order-container" sx={{ backgroundColor: '#FFFFFF', borderRadius: 1, m: 10, border: 1, borderColor: '#A3A29E', padding: 5 }}>
-                    <Typography>
-                        Your Order
-                    </Typography>
-                    <Typography>
-
-                    </Typography>
-
-
-                </Box>
+                    <Grid container spacing={4}>
+                        <Grid item xs={12} lg={7}>
+                            <Typography component="h1" variant="h6" sx={{ letterSpacing: '4px' }}>
+                                YOUR ORDER
+                            </Typography>
+                            <Button sx={{ backgroundColor: '#1D1F2C', mt: 2, fontSize: 18, width: '15em', color: 'white' }} variant="text">GO TO PAYMENT</Button>
+                        </Grid> 
+                        <Grid item xs={12} lg={5}>
+                            <Grid item lg={12}>
+                                <Button sx={{ backgroundColor: '#1D1F2C', mb: 1, fontSize: 18, width: '20em', color: 'white'  }} variant="text" size="large">ORDER MORE FOOD?</Button>
+                            </Grid>
+                            <Grid item lg={12}>             
+                                <Button sx={{ backgroundColor: '#1D1F2C', mb: 1, fontSize: 18, width: '20em', color: 'white'  }} variant="text" size="large">WANT ANOTHER SIDE DISH?</Button>
+                            </Grid>
+                            <Grid item lg={12}>    
+                                <Button sx={{ backgroundColor: '#1D1F2C', fontSize: 18, width: '20em', color: 'white'  }} variant="text" size="large">FORGOT TO ADD DRINKS?</Button> 
+                            </Grid>
+                        </Grid>     
+                    </Grid>
+                </Box>   
             </Container>
         </>
     );
